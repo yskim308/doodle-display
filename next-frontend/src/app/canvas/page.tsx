e7073fb8e4b98a5f691307ba02b121dcfecf6bdc
Young Seo Kim <youngseokim7@gmail.com>
2025-08-07 18:38:03 +0900

    inital canvas

--- next-frontend/src/app/canvas/page.tsx
+++ next-frontend/src/app/canvas/page.tsx
@@ -1,9 +1,35 @@
 "use client";
+import { useRef } from "react";
 import CanvasDraw from "react-canvas-draw";
+import { Button } from "@mui/material";
 export default function Page() {
+  const canvasRef = useRef<CanvasDraw | null>(null);
+
+  const handleSave = () => {
+    if (!canvasRef.current) return;
+    const saveData = canvasRef.current.getSaveData();
+    console.log(saveData);
+    console.log("save pressed");
+    canvasRef.current.clear();
+  };
+
+  const handleReset = () => {
+    if (!canvasRef.current) return;
+    canvasRef.current.clear();
+  };
+
   return (
     <div className="flex flex-col items-center">
-      <CanvasDraw brushRadius={1} lazyRadius={0} />
+      <h1 className="text-4xl my-3">draw... or else</h1>
+      <CanvasDraw ref={canvasRef} brushRadius={1} lazyRadius={0} />
+      <div>
+        <Button variant="contained" onClick={handleReset}>
+          reset
+        </Button>
+        <Button variant="contained" onClick={handleSave}>
+          submit
+        </Button>
+      </div>
     </div>
   );
 }
