3521657148891820c9507cf7154950c03105bce9
Young Seo Kim <youngseokim7@gmail.com>
2025-08-08 17:53:45 +0900

    use circularArray class to insert

--- hono-backend/src/index.ts
+++ hono-backend/src/index.ts
@@ -1,11 +1,30 @@
 import { serve } from "bun";
 import { Hono } from "hono";
+import circularArray from "./CircularArray";
 import type { Context } from "hono";
+import type { Canvas, ImageObject } from "./types";
 
 const app = new Hono();
-
 app.get("/", (c: Context) => {
   return c.text("Hewwo!");
+});
+
+// submit body
+interface SubmitBody {
+  canvas: Canvas;
+}
+app.post("/submit", async (c: Context) => {
+  const { canvas }: SubmitBody = await c.req.json();
+  if (!canvas) {
+    c.text("missing canvas field", 400);
+  }
+  const uniqueId = crypto.randomUUID();
+  const imageObject: ImageObject = {
+    imageId: uniqueId,
+    canvas: canvas,
+  };
+  circularArray.insert(imageObject);
+  c.text("inserted succesfully", 200);
 });
 
 serve({
