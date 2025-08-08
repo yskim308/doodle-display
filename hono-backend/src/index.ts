93bdf353dbd5141e8237678c522a94b579704252
Young Seo Kim <youngseokim7@gmail.com>
2025-08-08 17:26:50 +0900

    add typescript

--- hono-backend/src/index.ts
+++ hono-backend/src/index.ts
@@ -1,9 +1,10 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import type { Context } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c: Context) => {
+  return c.text("Hewwo!");
+});
 
-export default app
+export default app;
