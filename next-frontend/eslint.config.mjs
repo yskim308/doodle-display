afe8ce060b0abc54f3e258ca0c092e754ef29338
starmarb <sammi.kwon@yale.edu>
2025-08-25 17:41:37 +0900

    hi

--- next-frontend/eslint.config.mjs
+++ next-frontend/eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+    // 👇 Add this object to your config array
+  {
+    rules: {
+      // Disables the rule that bans using "any"
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
+
 
 export default eslintConfig;
